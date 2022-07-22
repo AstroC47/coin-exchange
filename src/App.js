b800b92148905f449614ed1193b66830c404d2f4
AstroC47 <58141091+AstroC47@users.noreply.github.com>
2022-07-22 21:54:44 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Coin from './components/coin/coin'
+import logo from './logo.svg';
+
+let sum = 0;
+for(let num of [1,2,3,4,5]) {
+   sum += num;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img className="App-logo" src={logo} alt="logo"/>
+      <h1> Coin Exchange</h1>
       </header>
+      <table className="coin-table">
+        <thead>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+        </thead>
+        <tbody id="root">
+        <Coin name="Bitcoin" ticker="BTC" price={48344}/>
+        <Coin name="Ethereum" ticker="ETH" price={2300}/>
+        <Coin name="Tether" ticker="USDT" price={2300}/>
+        {/* <AddCoin name="Ethereum" ticker="ETH" price="$3,345"/> */}
+        {/* <ArrowCoin name="Cardano" ticker="ADA" price="$0.5"/> */}
+        </tbody>
+    </table>
+      
     </div>
-  );
+  ); 
 }
 
 export default App;
